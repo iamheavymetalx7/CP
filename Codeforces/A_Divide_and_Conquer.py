607c9def4eee914637db28a78f2eb02c01d06b6b
iamheavymetalx7 <nitishkumarmay30@gmail.com>
2022-12-19 11:09:31 +0530

    "Edits Added"

---
+++ Codeforces/A_Divide_and_Conquer.py
@@ -0,0 +1,73 @@
+import sys, threading
+import math
+import time
+from os import path
+from collections import defaultdict, Counter, deque
+from bisect import *
+from string import ascii_lowercase
+from functools import cmp_to_key
+import heapq
+ 
+ 
+# # # # # # # # # # # # # # # #
+#       JAI SHREE RAM         #
+# # # # # # # # # # # # # # # #
+ 
+ 
+def lcm(a, b):
+    return (a*b)//(math.gcd(a,b))
+ 
+ 
+si= lambda:str(input())
+ii = lambda: int(input())
+mii = lambda: map(int, input().split())
+lmii = lambda: list(map(int, input().split()))
+i2c = lambda n: chr(ord('a') + n)
+c2i = lambda c: ord(c) - ord('a')
+    
+    
+def solve(t):
+    n=ii()
+    arr=lmii()
+
+    if sum(arr)%2==0:
+        print(0)
+    else:
+        maxi=math.inf
+        for i in range(n):
+            count = 0
+            if arr[i]%2==0:
+                while arr[i]!=1:
+                    arr[i]=arr[i]//2
+                    count+=1
+            else:
+                while(arr[i]%2!=0):
+                    arr[i]=arr[i]//2
+                    count+=1
+            if count!=0:
+                maxi=max(maxi,count)
+
+        print(maxi)
+
+    
+
+    
+def main():
+    t = 1
+    if path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt"):
+        sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
+        sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w')
+        start_time = time.time()
+        print("--- %s seconds ---" % (time.time() - start_time))
+ 
+ 
+    sys.setrecursionlimit(10**5)
+ 
+    t = int(input())
+ 
+    for i in range(t):
+        z= solve(i+1)
+        print(z)
+ 
+if __name__ == '__main__':
+    main()
