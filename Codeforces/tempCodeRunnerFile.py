583c03ac1c22ca01698cdda593dff0b18aff272b
iamheavymetalx7 <nitishkumarmay30@gmail.com>
2023-01-04 17:04:50 +0530

    update

--- Codeforces/tempCodeRunnerFile.py
+++ Codeforces/tempCodeRunnerFile.py
@@ -1,8 +1,2 @@
-    if path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt"):
-        sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
-        sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w')
-        start_time = time.time()
-        print("--- %s seconds ---" % (time.time() - start_time))
- 
- 
-    sys.setrecursionlimit(10**5)
+
+    ans=0
