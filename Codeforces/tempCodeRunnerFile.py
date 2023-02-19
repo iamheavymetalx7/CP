a1b9c2085ec54ac797da1366795dc3a54fd7aa39
iamheavymetalx7 <nitishkumarmay30@gmail.com>
2023-02-20 00:11:01 +0530

    "binary search"

--- Codeforces/tempCodeRunnerFile.py
+++ Codeforces/tempCodeRunnerFile.py
@@ -1,6 +1,1 @@
-    if path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt"):
-        sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
-        sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w')
-        start_time = time.time()
-        print("--- %s seconds ---" % (time.time() - start_time))
- 
+                # print("YES")
