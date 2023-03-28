e640b036c42138eb529b8f3d58e0166c72186602
iamheavymetalx7 <nitishkumarmay30@gmail.com>
2023-03-29 00:38:39 +0530

    "updates"

--- Codeforces/tempCodeRunnerFile.py
+++ Codeforces/tempCodeRunnerFile.py
@@ -1,2 +1,6 @@
-    print(fir)
-    print(sec)
+
+if(os.path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt")):
+    sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
+    sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w') 
+else:
+    input = io.BytesIO(os
