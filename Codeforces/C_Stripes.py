960856970a0580d47625ee81c733dfc4b5a68a91
iamheavymetalx7 <nitishkumarmay30@gmail.com>
2023-04-12 08:25:22 +0530

    "update"

--- Codeforces/C_Stripes.py
+++ Codeforces/C_Stripes.py
@@ -1,35 +1,13 @@
-import os
-import sys
-import threading,io 
-
-# if(os.path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt")):
-#     sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
-#     sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w') 
-# else:
-#     input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
-    
-def solve():
-    a=[]
-    for _ in range(8):
-        a.append(list(str(input())))
-
-    for i in range(8):
-        if a[i]==["R"]*8:
-            print("R")
-            return
-        if a[i]==["B"]*8:
-            print("B")
-            return
-    for i in range(8):
-        if [row[i] for row in a]==["R"]*8:
-            print("R")
-            return
-        if [row[i] for row in a]==["B"]*8:
-            print("B")
-            return
-        
-
-
-t=int(input())
+t = int(input())
 for _ in range(t):
-    solve()
+    input()
+    l = []
+    for i in range(8):
+        l.append(input())
+    ans = 'B'
+    for i in range(8):
+        if(l[i] == 8 * 'R'):
+            ans = 'R'
+        
+    
+    print(ans)
--- Codeforces/tempCodeRunnerFile.py
+++ Codeforces/tempCodeRunnerFile.py
@@ -1,6 +1,2 @@
-
-if(os.path.exists("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt")):
-    sys.stdin = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/input.txt", 'r')
-    sys.stdout = open("/Users/nitishkumar/Documents/Template_Codes/Python/CP/Codeforces/output.txt", 'w') 
-else:
-    input = io.BytesIO(os
+nt(arr)
+    # print(mattt)
