fb2e15b5d656c40065bd6101e661f52328382961
iamheavymetalx7 <nitishkumarmay30@gmail.com>
2023-11-09 02:07:23 +0530

    "commit"

--- Codeforces/guess_permutation_hackerearth.py
+++ Codeforces/guess_permutation_hackerearth.py
@@ -141,14 +141,14 @@ def solve():
             curr=i
     # print(perm)
             
-    # for i in range(n-1,idx+1,-1):
-    #     # print(a[i],idx)
-    #     if a[i]==idx+1:
-    #         perm[i]=n-1
-    #         right.discard(n-1)
-    #         tot.discard(n-1)
+    for i in range(n-1,idx+1,-1):
+        # print(a[i],idx)
+        if a[i]==idx+1:
+            perm[i]=n-1
+            right.discard(n-1)
+            tot.discard(n-1)
 
-    #         break
+            break
     # print(perm,"here")
     
     for i in range(n):
