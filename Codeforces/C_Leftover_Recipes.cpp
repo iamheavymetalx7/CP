1c11130425cf9637acb804bfb733ef3eabf7074c
iamheavymetalx7 <nitishkumarmay30@gmail.com>
2024-06-13 21:23:25 +0530

    "contest again streak"

---
+++ Codeforces/C_Leftover_Recipes.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+    using namespace std;
+    #define int long long
+    #define pi (3.141592653589)
+    #define mod 1000000007
+    #define float double
+    #define ff first
+    #define ss second
+    #define mk make_pair
+    #define pb push_back
+    #define rep(i, start, end) for (int i = start; i < end; i++)
+    #define ld long double
+    #define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+    int inf = 1000000000000000000;
+    using ii = pair<int, int>;
+    
+    void solve()
+    {
+        int n;
+        cin>>n;
+        // cout<<n<<endl;
+        vector<vector<int>> vec(3,vector<int>(n,0));
+
+
+        for(int i=0;i<3;i++){
+            for(int j=0;j<n;j++){
+                cin>>vec[i][j];
+            }
+        }
+        
+
+    
+    }
+    
+    signed main()
+    {
+        fast
+        // int t; cin>>t; while(t--)
+    
+        #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+        #endif
+        solve();
+        return 0;
+    }
